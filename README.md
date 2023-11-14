59479f96d340649af20fce68be9377e334c7ea75
Justyn Rodrigues <77762513+becausejustyn@users.noreply.github.com>
2023-11-14 11:55:46 +1100

    converting data for arrow

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # spotify_things
+
+
+```r
+stream_hist <- purrr::map_df(c(0:2), function(x) {
+  jsonlite::fromJSON(
+    glue::glue('data/endsong_{x}.json')
+  )
+}) |> 
+  select(-c(username, platform, conn_country:user_agent_decrypted, episode_name:spotify_episode_uri, offline, incognito_mode)) |> 
+  rename_with(~ gsub('master_metadata_', '', .x)) 
+```
---
+++ prep.R
@@ -0,0 +1,30 @@
+
+#list.files('data')
+
+library(jsonlite)
+library(dplyr)
+library(purrr)
+library(glue)
+library(arrow)
+
+# Create the 'parq' directory if it doesn't exist
+dir.create("data/parq", showWarnings = FALSE, recursive = TRUE)
+
+FILE_PATHS <- glue::glue('data/endsong_{0:2}.json')
+
+FILE_PATHS |>
+  walk(~{
+    df <- jsonlite::fromJSON(.x) |>
+      dplyr::select(-c(username, platform, conn_country:user_agent_decrypted, episode_name:spotify_episode_uri, offline, incognito_mode)) |>
+      dplyr::rename_with(~ gsub('master_metadata_', '', .x))
+    
+    # Remove the ".json" extension from the output file name
+    output_file <- gsub("\\.json$", "", basename(.x))
+    
+    # Write the Parquet file to the 'data/parq' directory
+    write_parquet(df, glue::glue('data/parq/{output_file}.parquet'))
+  })
+
+
+df <- open_dataset("data/parq/") |>
+  collect()
